6f8771dc100975047beda48c064e081358e132d6
gautam2003kumar <gautamkishor09072003@gmail.com>
2024-10-21 15:49:35 +0530

    some change in subscription model

--- backend/src/models/subscription.model.js
+++ backend/src/models/subscription.model.js
@@ -4,13 +4,18 @@ import mongoose , {Schema} from "mongoose";
 const subscriptionSchema = new Schema({
     subscriber:{
         type: Schema.Types.ObjectId, // one who is subscribing
-        ref: "User"
+        ref: "User",
+        require: true
     },
     channel: {
         type: Schema.Types.ObjectId, // one to whon 'subscriber' is subscribing
-        ref: "User"
+        ref: "User",
+        require: truec
     }
 },{timestamps: true})
 
+
+// Ensure a user can't subscribe to the same channel multiple times
+subscriptionSchema.index({ subscriber: 1, channel: 1 }, { unique: true });
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema)
